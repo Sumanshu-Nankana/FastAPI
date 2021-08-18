47fbbd8b0db537c42b18d9d74865a8f1bf161bee
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-08-18 02:10:21 +0100

    env vars

---
+++ learn/007_env_var.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from config import setting
+
+tags_metadata = [
+            {
+                "name" : "user",
+                "description": "This is user route"
+            },
+            {
+                "name" : "products",
+                "description" : "This is product route"
+            }
+]
+
+app = FastAPI(
+             title=setting.TITLE,
+             version=setting.VERSION,
+             description=setting.DESCRIPTION,
+             openapi_tags=tags_metadata,
+             contact = {"name" : setting.NAME,
+                "email" : setting.EMAIL},
+             redoc_url=None)
+
+@app.get('/users', tags=["user"])
+def get_users():
+    return {"message" : "hello user"}
+
+@app.get('/items', tags=["products"])
+def get_items():
+    return {"message" : "hello items"}
+
+@app.get('/getenvvar', tags=["config"])
+def get_envvars():
+    return {"database" : setting.DATABASE_URL}
+
+
+
