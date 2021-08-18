e81f0eaad778a8b4d53f58ca5a35c37663da9f68
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-08-18 02:03:06 +0100

    Setting Environment Variables

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,19 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
 	PROJECT_TITLE : str = "Jobboard"
 	PROJECT_VERSION : str = "0.0.1"
+
+	POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+	POSTGRES_PASSWORD : str = os.getenv("POSTGRES_PASSWORD")
+	POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER", "localhost")
+	POSTGRES_PORT : str = os.getenv("POSTGRES_PORT", 5432)
+	POSTGRES_DB : str = os.getenv("POSTGRES_DB", "db_jobcard")
+	DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES:PORT}/{POSTGRES_DB}"_
 
 settings = Settings()
