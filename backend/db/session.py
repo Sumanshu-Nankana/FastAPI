032095344a442defead7e610c320937387647599
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-08-20 12:47:22 +0100

    SqLite database connection

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,7 +2,17 @@ from core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# For postgrSQL database
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# For SQlLite Database
+# By default SQLite will only allow one thread to communicate.
+# But in FastAPI, more than one thread can interact with the database
+# Thus we need to override default parameter and thus setting check_same_thread : False
+# This is required only for SQLite database
+
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
+#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- learn/database.py
+++ learn/database.py
@@ -3,9 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# For PostgreSQL Database
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# For SQLite Database
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./sqlite.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread" : False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
