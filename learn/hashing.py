be75b98149f5391084ac7ac436edddc05b3ac9cd
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-08-21 22:39:02 +0100

    Password Hashing using Passlib

---
+++ learn/hashing.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+
+class Hasher:
+
+	@staticmethod
+	def get_hash_password(plain_password):
+		return pwd_context.hash(plain_password)
+	
+	@staticmethod	
+	def verify_password(plain_password, hash_password):
+		return pwd_context.verify(plain_password, hash_password)
