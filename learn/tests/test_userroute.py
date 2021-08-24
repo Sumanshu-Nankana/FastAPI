2eb4eebc8aecaddb9943ee96737170996408ef02
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-08-24 18:25:14 +0100

    Testing the database - Dependency Override

--- learn/tests/test_userroute.py
+++ learn/tests/test_userroute.py
@@ -1,21 +1,32 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 import sys
 import os
 import json
 
-#file = os.path.abspath(__file__)
-#print(f"The value os current file is __file__ : {file}")
-
-#parent_dir = os.path.dirname(file)
-#print(f"The value of patent_dir is : {parent_dir}")
-
-#parent_parent_dir = os.path.dirname(parent_dir)
-#print(f"The value of parent_parent_dir is : {parent_parent_dir}")
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from models import Base
+from database import get_db
 from main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+	try:
+		db = TestingSessionLocal()
+		yield db
+	finally:
+		db.close()
+		Base.metadata.drop_all(bind=engine)
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
