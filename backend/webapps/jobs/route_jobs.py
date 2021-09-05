213d58367de6bb58d7a8f9a98687a0d14c545839
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-09-05 12:05:17 +0100

    Detail view of Job and Item

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -3,6 +3,7 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from db.repository.jobs import list_jobs
+from db.repository.jobs import retrieve_job
 from sqlalchemy.orm import Session
 from db.session import get_db
 
@@ -13,3 +14,9 @@ router = APIRouter()
 def home(request:Request, db:Session=Depends(get_db)):
 	jobs = list_jobs(db=db)
 	return templates.TemplateResponse("jobs/homepage.html", {"request" : request, "jobs" : jobs})
+
+
+@router.get("/detail/{id}")
+def job_detail(id:int, request:Request, db:Session=Depends(get_db)):
+	job = retrieve_job(id=id, db=db)
+	return templates.TemplateResponse("jobs/detail.html", {"request" :request, "job" : job})
--- learn/webapps/routers/items.py
+++ learn/webapps/routers/items.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from models import Items
+from models import Items, User
 from sqlalchemy.orm import Session
 from database import get_db
 
@@ -11,3 +11,9 @@ templates = Jinja2Templates(directory="templates")
 def home_page(request: Request, db:Session=Depends(get_db)):
 	items = db.query(Items).all()
 	return templates.TemplateResponse("item_homepage.html", {"request" : request, "items" : items})
+
+@router.get("/detail/{id}")
+def item_detail(request: Request, id:int, db:Session=Depends(get_db)):
+	item = db.query(Items).filter(Items.id==id).first()
+	email = db.query(User).filter(User.id==item.owner_id).first().email
+	return templates.TemplateResponse("item_detail.html", {"request" : request, "item" : item, "email" : email})
