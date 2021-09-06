97be3f06832814cb51a4e1754c702201e27b0e65
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-09-06 21:58:30 +0100

    update Login

--- learn/webapps/routers/auth.py
+++ learn/webapps/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/login")
