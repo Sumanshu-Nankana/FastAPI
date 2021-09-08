907ccb7623bf85018b0fabd2c72828309076af6e
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-09-09 00:24:12 +0100

    Implement Delete Logic

--- learn/routers/login.py
+++ learn/routers/login.py
@@ -1,5 +1,6 @@
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends, HTTPException, status
+from utils import OAuth2PasswordBearerWithCookie
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
@@ -7,7 +8,7 @@ from hashing import Hasher
 from jose import jwt
 from config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
 
 
 router = APIRouter()
@@ -15,7 +16,9 @@ router = APIRouter()
 
 @router.post("/login/token")
 def retrieve_token_for_authenticated_user(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
 ):
     user = db.query(User).filter(User.email == form_data.username).first()
     if not user:
@@ -28,4 +31,5 @@ def retrieve_token_for_authenticated_user(
         )
     data = {"sub": form_data.username}
     jwt_token = jwt.encode(data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    response.set_cookie(key="access_token", value=f"Bearer {jwt_token}", httponly=True)
     return {"access_token": jwt_token, "token_type": "bearer"}
---
+++ learn/utils.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, List, Optional, Union
+
+from fastapi.security import OAuth2
+from fastapi.exceptions import HTTPException
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+
+class OAuth2PasswordBearerWithCookie(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        description: Optional[str] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(
+            flows=flows,
+            scheme_name=scheme_name,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        # authorization: str = request.headers.get("Authorization")
+        authorization: str = request.cookies.get("access_token")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
