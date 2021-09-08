c49807980965a7d0b41fdf953eaaf66aa0b2f65e
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-09-08 11:02:00 +0100

    Create Item & Job logic

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Depends
-from fastapi import Request
+from fastapi import Request, status, responses
+from fastapi.security.utils import get_authorization_scheme_param
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from db.repository.jobs import list_jobs
-from db.repository.jobs import retrieve_job
+from db.repository.jobs import retrieve_job, create_new_job
 from sqlalchemy.orm import Session
 from db.session import get_db
+from db.models.users import User
+from apis.version1.route_login import get_current_user_from_token
+from webapps.jobs.forms import JobCreateForm
+from schemas.jobs import JobCreate
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter()
@@ -30,3 +35,26 @@ def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
 @router.get("/post-a-job")
 def create_job(request: Request):
     return templates.TemplateResponse("jobs/create_job.html", {"request": request})
+
+
+@router.post("/post-a-job")
+async def create_job(request: Request, db: Session = Depends(get_db)):
+    form = JobCreateForm(request)
+    await form.load_data()
+    if form.is_valid():
+        try:
+            token = request.cookies.get("access_token")
+            scheme, param = get_authorization_scheme_param(token)
+            current_user: User = get_current_user_from_token(token=param, db=db)
+            job = JobCreate(**form.__dict__)
+            job = create_new_job(job=job, db=db, owner_id=current_user.id)
+            return responses.RedirectResponse(
+                f"/detail/{job.id}", status_code=status.HTTP_302_FOUND
+            )
+        except Exception as e:
+            print(e)
+            form.__dict__.get("errors").append(
+                "You might not be logged in,In case problem persists, please contact us."
+            )
+            return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
+    return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
--- learn/webapps/routers/items.py
+++ learn/webapps/routers/items.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, responses, status
 from fastapi.templating import Jinja2Templates
 from models import Items, User
 from sqlalchemy.orm import Session
 from database import get_db
+from jose import jwt
+from config import settings
+from datetime import datetime
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
@@ -28,3 +31,61 @@ def item_detail(request: Request, id: int, db: Session = Depends(get_db)):
 @router.get("/create-an-item")
 def create_an_item(request: Request):
     return templates.TemplateResponse("create_item.html", {"request": request})
+
+
+@router.post("/create-an-item")
+async def create_an_item(request: Request, db: Session = Depends(get_db)):
+    form = await request.form()
+    title = form.get("title")
+    description = form.get("description")
+    errors = []
+    if not title or len(title) < 4:
+        errors.append("Title should be > 4 chars")
+    if not description or len(description) < 10:
+        errors.append("Description should be > 10 chars")
+    if len(errors) > 0:
+        return templates.TemplateResponse(
+            "create_item.html", {"request": request, "errors": errors}
+        )
+    try:
+        token = request.cookies.get("access_token")
+        if not token:
+            errors.append("Kindly Authenticate first by login")
+            return templates.TemplateResponse(
+                "create_item.html", {"request": request, "errors": errors}
+            )
+        scheme, _, param = token.partition(" ")
+        payload = jwt.decode(param, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        email = payload.get("sub")
+        if email is None:
+            errors.append("Kindly login first, you are not authenticated")
+            return templates.TemplateResponse(
+                "create_item.html", {"request": request, "errors": errors}
+            )
+        else:
+            user = db.query(User).filter(User.email == email).first()
+            if user is None:
+                errors.append("You are not authenticated, Kindly Login")
+                return templates.TemplateResponse(
+                    "create_item.html", {"request": request, "errors": errros}
+                )
+            else:
+                item = Items(
+                    title=title,
+                    description=description,
+                    date_posted=datetime.now().date(),
+                    owner_id=user.id,
+                )
+                db.add(item)
+                db.commit()
+                db.refresh(item)
+                print(item.id)
+                return responses.RedirectResponse(
+                    f"/detail/{item.id}", status_code=status.HTTP_302_FOUND
+                )
+    except Exception as e:
+        errors.append("Something is wrong !")
+        print(e)
+        return templates.TemplateResponse(
+            "create_item.html", {"request": request, "errors": errors}
+        )
